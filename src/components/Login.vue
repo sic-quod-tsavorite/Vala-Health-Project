40ed985cf7f6eb196d2632689222e7df9cd526b9
Larsh95 <lars7537@easv365.dk>
2020-05-22 13:39:52 +0200

    loginbg

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app id="lbg">
+    <div>
+      <v-card>
+        <v-card-title>
+          Login
+        </v-card-title>
+      </v-card>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@media (min-width: 1024px) {
+  #lbg {
+    background: url(../assets/loginbg.png) no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+}
+@media (max-width: 1024px) {
+  #lbg {
+    background: url(../assets/loginbg.png) no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+}
+</style>
